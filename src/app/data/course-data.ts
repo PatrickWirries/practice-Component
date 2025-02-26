3f080cc3f581faff75d43aef8a58a27afbc168b3
PatrickWirries <patrickwirries@outlook.com>
2025-02-26 11:05:19 -0600

    Added a few components - Different courses

--- src/app/data/course-data.ts
+++ src/app/data/course-data.ts
@@ -3,15 +3,15 @@ import {Course} from "../model/course";
 export const COURSES:Course[] = [
     {
         id: 1,
-        title : "CSci 160: Computer Science - I",
+        title : "CSCI 160: Computer Science - I",
         url: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
-        description: "A introductory programming class",
+        description: "An introductory programming class",
         level: '100 level',
         credits: 4
     },
     {
         id: 2,
-        title: "CSci 161: Computer Science - II",
+        title: "CSCI 161: Computer Science - II",
         url: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
         description: "Introduction to data structures, algorithms and program complexity",
         level: '100 level',
@@ -20,7 +20,7 @@ export const COURSES:Course[] = [
 
     {
         id: 3,
-        title: 'CSci 213: Modern Software Development ',
+        title: 'CSCI 213: Modern Software Development ',
         url: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
         description: "Introduction to modern software developemnt. C#",
         level: '200 level',
@@ -29,7 +29,7 @@ export const COURSES:Course[] = [
 
     {
         id: 4,
-        title: "CSci 313: Advanced Software Development",
+        title: "CSCI 313: Advanced Software Development",
         url: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
         description: "Introduction to modern software frameworks",
         level: '200 level',
@@ -37,7 +37,7 @@ export const COURSES:Course[] = [
     },
     {
         id: 5,
-        title: 'CSci 222: Discrete Math',
+        title: 'CSCI 222: Discrete Math',
         url: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
         description: "Discrete Math",
         level: '200 level',
@@ -45,7 +45,7 @@ export const COURSES:Course[] = [
     },
     {
         id: 6,
-        title: 'CSci 374: Computer Organization',
+        title: 'CSCI 374: Computer Organization',
         url: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
         description: 'Computer Architecture and organization',
         level: '300 level',
